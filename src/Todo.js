cecd7ee09b2553c7af1a7b736c52ce83f590e9d4
Edyesid <edyesid2000@gmail.com>
2021-02-08 00:39:48 -0500

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.props.text}</h1>
+                <h1>{this.props.priority}</h1>
+                <h1>{this.props.dueDate}</h1>
+            </div>
+        );
+    }
+}
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Todo} from './Todo';
+
+export class TodoList extends React.Component {
+    render() {
+        const listTodo = this.props.TodoList.map
+        return 
+    }
+}
